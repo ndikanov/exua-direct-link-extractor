be83927d3160d9771f31925a7f9a18e18175aad1
ndikanov <ndikanov@users.noreply.github.com>
2016-10-31 21:52:27 +0200

    now it works and copies direct links

--- exua_link_extractor.user.js
+++ exua_link_extractor.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ExUa Direct Link Extractor
 // @namespace    http://github.com/ndikanov/
-// @version      0.1
+// @version      0.2
 // @description  Direct link extractor for ex.ua
 // @author       Nick
 // @grant        none
@@ -12,31 +12,73 @@
 
 function main() {
 
-    var sourceElements = document.getElementsByClassName('fox-play-btn');
+    var sourceElements = document.getElementsByClassName('mail-add');
 
-    for(var i=1; i<sourceElements.length; i++){
-        sourceElement = sourceElements[i];
+    for(var i=0; i<sourceElements.length; i++){
 
-        var getLink = sourceElement.getAttribute('href');
-        var showLink = getLink.replace('get', 'show');
-        
-        var directLinkInput = document.createElement('input');
-        directLinkInput.setAttribute('value', 'www.ex.ua'+showLink);
-        
-        var directLinkButton = document.createElement('button');
-        directLinkButton.innerHTML = 'копировать ссылку';
-        directLinkButton.addEventListener("click", copyDirectLink);
+        var md5 = sourceElements[i].parentNode.previousSibling.children[1].innerHTML;
+        var fileNameElement = sourceElements[i].parentNode.parentNode.previousElementSibling.previousElementSibling.getElementsByTagName('a')[0];
+        var fileName = fileNameElement.getAttribute('title');
+        fileName = encodeURI(fileName);
 
-        var parent = sourceElement.parentNode;    
-        parent.insertBefore(directLinkButton, sourceElement.nextSibling);
-        parent.insertBefore(directLinkInput, sourceElement.nextSibling);
-    
-    }
-    
-    function copyDirectLink(){
-        var currentInput = this.previousElementSibling;
-        currentInput.select();
-        document.execCommand('copy');
+        var fsElements = sourceElements[i].parentNode.nextSibling.children;
+
+        var span = document.createElement('span');
+        span.setAttribute('class', 'r_button_small');
+        span.style.float = 'right';
+
+        var parent = fileNameElement.parentNode;
+        parent.insertBefore(span, fileNameElement.nextSibling);
+
+        for(var j=0; j<fsElements.length; j++){
+            var fsElement = fsElements[j];
+            
+            var fsName = fsElement.innerHTML;
+            var fileId = fsElement.getAttribute('href').match(/\d+/);
+
+            var directLink = 'http://'+fsName+'.www.ex.ua/show/'+md5+'/'+fileId+'/'+fileName;
+
+            var directLinkInput = document.createElement('input');
+            directLinkInput.setAttribute('value', directLink);
+
+            // ===TextArea Hiding Begin===
+            // Place in top-left corner of screen regardless of scroll position.
+            directLinkInput.style.position = 'fixed';
+            directLinkInput.style.top = 0;
+            directLinkInput.style.left = 0;
+
+            // Ensure it has a small width and height. Setting to 1px / 1em
+            // doesn't work as this gives a negative w/h on some browsers.
+            directLinkInput.style.width = '2em';
+            directLinkInput.style.height = '2em';
+
+            // We don't need padding, reducing the size if it does flash render.
+            directLinkInput.style.padding = 0;
+
+            // Clean up any borders.
+            directLinkInput.style.border = 'none';
+            directLinkInput.style.outline = 'none';
+            directLinkInput.style.boxShadow = 'none';
+
+            // Avoid flash of white box if rendered for any reason.
+            directLinkInput.style.background = 'transparent';
+            // ===TextArea Hiding End===
+
+            var directLinkButton = document.createElement('a');
+            directLinkButton.innerHTML = 'копировать '+fsName;
+            directLinkButton.setAttribute('href','');
+            directLinkButton.setAttribute('onclick','return false');
+            directLinkButton.addEventListener("click", copyDirectLink);
+
+            span.appendChild(directLinkInput);
+            span.appendChild(directLinkButton);
+            
+            function copyDirectLink(){
+                var currentInput = this.previousElementSibling;
+                currentInput.select();
+                document.execCommand('copy');
+            }
+        }
     }
 }
 
