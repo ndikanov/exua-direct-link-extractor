bc582550dffa7e7167661ebdc70772135bf72f67
ndikanov <ndikanov@users.noreply.github.com>
2016-10-31 23:54:23 +0200

    Direct links list added

--- exua_link_extractor.user.js
+++ exua_link_extractor.user.js
@@ -1,93 +1,111 @@
-// ==UserScript==
-// @name         ExUa Direct Link Extractor
-// @namespace    http://github.com/ndikanov/
-// @version      0.2.1
-// @description  Direct link extractor for ex.ua
-// @author       Nick
-// @grant        none
-// @include      http://www.ex.ua/*
-// @include      http://ex.ua/*
-// ==/UserScript==
+    // ==UserScript==
+    // @name         ExUa Direct Link Extractor
+    // @namespace    http://github.com/ndikanov/
+    // @version      0.3
+    // @description  Direct link extractor for ex.ua
+    // @author       Nick
+    // @grant        none
+    // @include      http://www.ex.ua/*
+    // @include      http://ex.ua/*
+    // ==/UserScript==
 
 
-function main() {
+    function main() {
 
-    var sourceElements = document.getElementsByClassName('mail-add');
+        var sourceElements = document.getElementsByClassName('mail-add');
+        var directLinks = [];
 
-    
+        for(var i=0; i<sourceElements.length; i++){
+            var playColumnChildrenQty = sourceElements[i].parentNode.parentNode.previousElementSibling.childNodes.length;
 
-    for(var i=0; i<sourceElements.length; i++){
+            if (playColumnChildrenQty > 1) {
+                var md5 = sourceElements[i].parentNode.previousElementSibling.children[1].innerHTML;
+                var fileNameElement = sourceElements[i].parentNode.parentNode.previousElementSibling.previousElementSibling.getElementsByTagName('a')[0];
+                var fileName = fileNameElement.getAttribute('title');
+                fileName = encodeURI(fileName);
 
-        var playColumnChildrenQty = sourceElements[i].parentNode.parentNode.previousElementSibling.childNodes.length;
-        console.log(playColumnChildrenQty);
+                var fsElements = sourceElements[i].parentNode.nextSibling.children;
 
-        if (playColumnChildrenQty > 1) {
+                var span = document.createElement('span');
+                span.setAttribute('class', 'r_button_small');
+                span.style.float = 'right';
 
-            var md5 = sourceElements[i].parentNode.previousElementSibling.children[1].innerHTML;
-            var fileNameElement = sourceElements[i].parentNode.parentNode.previousElementSibling.previousElementSibling.getElementsByTagName('a')[0];
-            var fileName = fileNameElement.getAttribute('title');
-            fileName = encodeURI(fileName);
+                var parent = fileNameElement.parentNode;
+                parent.insertBefore(span, fileNameElement.nextSibling);
 
-            var fsElements = sourceElements[i].parentNode.nextSibling.children;
+                for(var j=0; j<fsElements.length; j++){
+                    var fsElement = fsElements[j];
+                    
+                    var fsName = fsElement.innerHTML;
+                    var fileId = fsElement.getAttribute('href').match(/\d+/);
 
-            var span = document.createElement('span');
-            span.setAttribute('class', 'r_button_small');
-            span.style.float = 'right';
+                    var directLink = 'http://'+fsName+'.www.ex.ua/show/'+md5+'/'+fileId+'/'+fileName;
+                    // console.log(directLink);
 
-            var parent = fileNameElement.parentNode;
-            parent.insertBefore(span, fileNameElement.nextSibling);
+                    if (j==0) {
+                        directLinks.push(directLink);
+                    }
 
-            for(var j=0; j<fsElements.length; j++){
-                var fsElement = fsElements[j];
-                
-                var fsName = fsElement.innerHTML;
-                var fileId = fsElement.getAttribute('href').match(/\d+/);
+                    var directLinkInput = document.createElement('input');
+                    directLinkInput.setAttribute('value', directLink);
 
-                var directLink = 'http://'+fsName+'.www.ex.ua/show/'+md5+'/'+fileId+'/'+fileName;
+                    // ===TextArea Hiding Begin===
+                    // Place in top-left corner of screen regardless of scroll position.
+                    directLinkInput.style.position = 'fixed';
+                    directLinkInput.style.top = 0;
+                    directLinkInput.style.left = 0;
 
-                var directLinkInput = document.createElement('input');
-                directLinkInput.setAttribute('value', directLink);
+                    // Ensure it has a small width and height. Setting to 1px / 1em
+                    // doesn't work as this gives a negative w/h on some browsers.
+                    directLinkInput.style.width = '2em';
+                    directLinkInput.style.height = '2em';
 
-                // ===TextArea Hiding Begin===
-                // Place in top-left corner of screen regardless of scroll position.
-                directLinkInput.style.position = 'fixed';
-                directLinkInput.style.top = 0;
-                directLinkInput.style.left = 0;
+                    // We don't need padding, reducing the size if it does flash render.
+                    directLinkInput.style.padding = 0;
 
-                // Ensure it has a small width and height. Setting to 1px / 1em
-                // doesn't work as this gives a negative w/h on some browsers.
-                directLinkInput.style.width = '2em';
-                directLinkInput.style.height = '2em';
+                    // Clean up any borders.
+                    directLinkInput.style.border = 'none';
+                    directLinkInput.style.outline = 'none';
+                    directLinkInput.style.boxShadow = 'none';
 
-                // We don't need padding, reducing the size if it does flash render.
-                directLinkInput.style.padding = 0;
+                    // Avoid flash of white box if rendered for any reason.
+                    directLinkInput.style.background = 'transparent';
+                    // ===TextArea Hiding End===
 
-                // Clean up any borders.
-                directLinkInput.style.border = 'none';
-                directLinkInput.style.outline = 'none';
-                directLinkInput.style.boxShadow = 'none';
+                    var directLinkButton = document.createElement('a');
+                    directLinkButton.innerHTML = 'копировать '+fsName;
+                    directLinkButton.setAttribute('href','');
+                    directLinkButton.setAttribute('onclick','return false');
+                    directLinkButton.addEventListener("click", copyDirectLink);
 
-                // Avoid flash of white box if rendered for any reason.
-                directLinkInput.style.background = 'transparent';
-                // ===TextArea Hiding End===
-
-                var directLinkButton = document.createElement('a');
-                directLinkButton.innerHTML = 'копировать '+fsName;
-                directLinkButton.setAttribute('href','');
-                directLinkButton.setAttribute('onclick','return false');
-                directLinkButton.addEventListener("click", copyDirectLink);
-
-                span.appendChild(directLinkInput);
-                span.appendChild(directLinkButton);
-                
-                function copyDirectLink(){
-                    var currentInput = this.previousElementSibling;
-                    currentInput.select();
-                    document.execCommand('copy');
+                    span.appendChild(directLinkInput);
+                    span.appendChild(directLinkButton);
+                    
+                    function copyDirectLink(){
+                        var currentInput = this.previousElementSibling;
+                        currentInput.select();
+                        document.execCommand('copy');
+                    }
                 }
             }
         }
-    }
-}
 
-window.onload = main();
+        //Direct Links List
+        var linksText = directLinks.join('\n');
+
+        var topCell = sourceElements[0].parentNode.parentNode.parentNode.previousElementSibling.getElementsByTagName('td')[0];
+        var details = document.createElement('details');
+        var summary = document.createElement('summary');
+        summary.style.padding = '5px 0';
+        summary.style.cursor = 'pointer';
+
+        var textArea = document.createElement('textarea');
+        textArea.value = linksText;
+        textArea.style.width = '100%';
+        textArea.rows = Math.min(15, directLinks.length);
+
+        topCell.appendChild(details).appendChild(summary).innerHTML='<b>Список прямых ссылок</b>';
+        details.appendChild(textArea);
+    }
+
+    window.onload = main();
